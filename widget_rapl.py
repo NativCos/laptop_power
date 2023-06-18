dfcf4be90ecaac0ccc9dbdcc1d567428c0e34ceb
Nikita <continuouslive@outlook.com>
2023-06-18 15:18:04 +0300

    switch from PySide to PyQt

--- widget_rapl.py
+++ widget_rapl.py
@@ -1,8 +1,8 @@
 # File: main.py
 import sys
-from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtCore import QFile, QIODevice, QTimer
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QTimer
 
 from service import IntelPowerCappingFramework
 
@@ -13,11 +13,7 @@ class RAPLWidget(QWidget):
         super(RAPLWidget, self).__init__()
 
         ui_file_name = "widget_rapl.ui"
-        ui_file = QFile(ui_file_name)
-        ui_file.open(QIODevice.ReadOnly)
-        loader = QUiLoader()
-        self.raplw = loader.load(ui_file, self)
-        ui_file.close()
+        uic.loadUi(ui_file_name, self)
 
         self.raplservice = IntelPowerCappingFramework()
 
@@ -26,44 +22,44 @@ class RAPLWidget(QWidget):
         self.timer.start(1000)
 
     def rapl_refresh(self):
-        self.raplw.lcdNumber_msr_pl1.display(
+        self.lcdNumber_msr_pl1.display(
             str(
                 self.raplservice.long_term.get_power_limit_uw() / float(10 ** 6)
             )
         )
-        self.raplw.lcdNumber_msr_pl1tw.display(
+        self.lcdNumber_msr_pl1tw.display(
             str(
                 self.raplservice.long_term.get_time_window_us() / float(10 ** 6)
             )
         )
-        self.raplw.lcdNumber_msr_pl2.display(
+        self.lcdNumber_msr_pl2.display(
             str(
                 self.raplservice.short_term.get_power_limit_uw() / float(10 ** 6)
             )
         )
-        self.raplw.lcdNumber_msr_pl2tw.display(
+        self.lcdNumber_msr_pl2tw.display(
             str(
                 self.raplservice.short_term.get_time_window_us() / float(10 ** 6)
             )
         )
         if self.raplservice.get_mmio_rapl_enabled():
-            self.raplw.checkBox_mmio.setChecked(True)
-        self.raplw.lcdNumber_mmio_pl1.display(
+            self.checkBox_mmio.setChecked(True)
+        self.lcdNumber_mmio_pl1.display(
             str(
                 self.raplservice.mmio.long_term.get_power_limit_uw() / float(10 ** 6)
             )
         )
-        self.raplw.lcdNumber_msr_pl1tw.display(
+        self.lcdNumber_msr_pl1tw.display(
             str(
                 self.raplservice.mmio.long_term.get_time_window_us() / float(10 ** 6)
             )
         )
-        self.raplw.lcdNumber_mmio_pl2.display(
+        self.lcdNumber_mmio_pl2.display(
             str(
                 self.raplservice.mmio.short_term.get_power_limit_uw() / float(10 ** 6)
             )
         )
-        self.raplw.lcdNumber_mmio_pl2tw.display(
+        self.lcdNumber_mmio_pl2tw.display(
             str(
                 self.raplservice.mmio.short_term.get_time_window_us() / float(10 ** 6)
             )
