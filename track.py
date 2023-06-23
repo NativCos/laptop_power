6e17396915baf4f382daf81ab10f09a2c85cef47
Nikita <continuouslive@outlook.com>
2023-06-24 02:44:05 +0300

    try make track async

--- track.py
+++ track.py
@@ -4,6 +4,53 @@ import asyncio
 from service import BatteryService
 
 
+class Timer:
+    def __init__(self, timeout, callback):
+        self._task = None
+        self._timeout = timeout
+        self._callback = callback
+
+    def start(self):
+        self._task = asyncio.ensure_future(self._job())
+        return self
+
+    async def _job(self):
+        while True:
+            await asyncio.sleep(self._timeout)
+            await self._callback()
+
+    def cancel(self):
+        self._task.cancel()
+
+
+class Tracking:
+    def __init__(self):
+        self._batteryservice = BatteryService()
+        self.db_session = DBSession()
+
+    def once_track_battery_async(self):
+        self.db_session.add(self._batteryservice.get())
+
+    def track(self, session):
+        delay = 1  # seconds
+        max_time_count = 10  # every seconds
+        max_time_count = max_time_count / delay
+        time_count = 0
+
+        try:
+            while True:
+                session.add(self._batteryservice.get())
+
+                if time_count == max_time_count:
+                    session.commit()
+                    time_count = 0
+                time_count += 1
+
+                await asyncio.sleep(delay)
+        except asyncio.CancelledError:
+            session.commit()
+
+
 async def track(session):
     delay = 1  # seconds
     max_time_count = 10  # every seconds
